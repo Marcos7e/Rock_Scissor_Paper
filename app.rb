6b8720f9c53f775524e7ffa6a86078e94c547e7a
vagrant <vagrant@kleer-csd-box>
2017-02-23 15:14:53 -0600

    commit

--- app.rb
+++ app.rb
@@ -3,3 +3,18 @@ require 'sinatra'
 get '/' do
     erb :main
 end
+
+post "/seleccionPiedra" do
+	@seleccion=params["seleccion"]
+   @seleccion.to_s
+end
+
+post "/seleccionTijera" do
+	@seleccion=params["seleccion"]
+     @seleccion.to_s
+end
+
+post "/seleccionPapel" do
+	@seleccion=params["seleccion"]
+     @seleccion.to_s
+end
--- features/main_v.feature
+++ features/main_v.feature
@@ -3,4 +3,9 @@ Feature: Piedra papel o tijera
 Scenario: entrar
 	Given voy a la pantalla principal
 	Then debe decir "Piedra, Papel o Tijera"
-	
+
+
+Scenario: boton piedra
+	Given voy a la pantalla principal
+	When presionar "btn_Piedra"
+	Then debe dar get
