efe925a2a23ab746895427e0164d5167e0c23cae
vagrant <vagrant@kleer-csd-box>
2017-02-23 19:35:33 -0300

    Agregado robotSelect como metodo de seleccion random

--- lib/logic_l.rb
+++ lib/logic_l.rb
@@ -16,7 +16,18 @@ class Logic
 			@select_human = "papel".uppercase	
 		when 'tijera'
 			@select_human = "tijera".uppercase		
+	end
 
+
+	def robotSelect 
+		@random = rand(0..2)
+	switch @random
+		when  0
+			@select_robot ="piedra".uppercase
+		when  1
+			@select_human = "papel".uppercase
+		when  2
+			@select_human = "tijera".uppercase
 	end
 
 end
