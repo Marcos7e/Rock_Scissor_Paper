4a8ab6ed0d92b1e7c65910bebb9f76eac5054214
vagrant <vagrant@kleer-csd-box>
2017-02-24 17:00:10 -0300

    Corrigiendo una metida de pata

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ end
 post "/seleccionPiedra" do
    @seleccion=params["seleccion"]  
    @seleccion = @@logic.userSelect(@seleccion.to_s)
-   @ronda = "Ronda:"+@@logic.ronda.to_s
+   @ronda = "Ronda:"+@@logic.ronda.to_s + " de 10"
    @puntajeUser = "<h3>Puntaje acumulado:</h3>Jugador: "+@@logic.getScoreHuman.to_s
    @puntajeRobot = "<br>Robot: "+@@logic.getScoreRobot.to_s
    erb :main
@@ -20,7 +20,7 @@ end
 post "/seleccionTijera" do
    @seleccion=params["seleccion"]
    @seleccion = @@logic.userSelect(@seleccion.to_s)
-   @ronda = "Ronda:"+@@logic.ronda.to_s
+   @ronda = "Ronda:"+@@logic.ronda.to_s + " de 10"
    @puntajeUser = "<h3>Puntaje acumulado:</h3>Jugador: "+@@logic.getScoreHuman.to_s
    @puntajeRobot = "<br>Robot: "+@@logic.getScoreRobot.to_s
    erb :main
@@ -29,7 +29,7 @@ end
 post "/seleccionPapel" do
    @seleccion=params["seleccion"]
    @seleccion = @@logic.userSelect(@seleccion.to_s)
-   @ronda = "Ronda:"+@@logic.ronda.to_s
+   @ronda = "Ronda:"+@@logic.ronda.to_s + " de 10"
    @puntajeUser = "<h3>Puntaje acumulado:</h3>Jugador: "+@@logic.getScoreHuman.to_s
    @puntajeRobot = "<br>Robot: "+@@logic.getScoreRobot.to_s
    erb :main
