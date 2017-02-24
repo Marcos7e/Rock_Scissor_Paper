74f1b74e16d8b5567a7e57255ed508693285fd26
vagrant <vagrant@kleer-csd-box>
2017-02-24 14:18:22 -0600

    iamgenes de PC

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra'
 require_relative './lib/logic_l'
 
+def imagenPC
+	'<p>Computadora: </br><img src="/img/#{@@logic.select_robot}.jpg" height="250" width="300"></p>' 
+end
 
 get '/' do   
  @@logic = Logic.new
@@ -11,7 +14,8 @@ end
 post "/seleccionPiedra" do
    @seleccion=params["seleccion"]  
    @seleccion = @@logic.userSelect(@seleccion.to_s)
-   @imagen='<img src="/img/piedra.jpg" height="250" width="300">'
+   @imagen='<p>Player: </br><img src="/img/piedra.jpg" height="250" width="300"></p>'
+   @imagenComputadora=imagenPC
    @ronda = "Ronda:"+@@logic.ronda.to_s + " de 10"
    @puntajeUser = "<h3>Puntaje acumulado:</h3>Jugador: "+@@logic.getScoreHuman.to_s
    @puntajeRobot = "<br>Robot: "+@@logic.getScoreRobot.to_s
@@ -22,7 +26,8 @@ end
 post "/seleccionTijera" do
    @seleccion=params["seleccion"]
    @seleccion = @@logic.userSelect(@seleccion.to_s)
-   @imagen='<img src="/img/tijera.png" height="250" width="300">'
+   @imagen='<p>Player: </br><img src="/img/tijera.png" height="250" width="300"></p>'
+   @imagenComputadora=imagenPC
    @ronda = "Ronda:"+@@logic.ronda.to_s + " de 10"
    @puntajeUser = "<h3>Puntaje acumulado:</h3>Jugador: "+@@logic.getScoreHuman.to_s
    @puntajeRobot = "<br>Robot: "+@@logic.getScoreRobot.to_s
@@ -32,10 +37,12 @@ end
 post "/seleccionPapel" do
    @seleccion=params["seleccion"]
    @seleccion = @@logic.userSelect(@seleccion.to_s)
-   @imagen='<img src="/img/papel.jpg" height="250" width="300">'
+   @imagen='<p>Player: </br><img src="/img/papel.jpg" height="250" width="300"></p>'
+   @imagenComputadora=imagenPC
    @ronda = "Ronda:"+@@logic.ronda.to_s + " de 10"
    @puntajeUser = "<h3>Puntaje acumulado:</h3>Jugador: "+@@logic.getScoreHuman.to_s
    @puntajeRobot = "<br>Robot: "+@@logic.getScoreRobot.to_s
+
    erb :main
 end
 
